32f486403463edd0a5879abbd5f8ffd836e00522
Z <zacharyk.ishmael@gmail.com>
2021-05-28 10:55:35 +0100

    PySnake can now render snake body coordinates on screen

--- PySnake.py
+++ PySnake.py
@@ -10,7 +10,7 @@ class Snake:
     head = 0 #collumn for head of snake
     x = 0 # key for x position of snake head
     y = 1 # key for y position of snake head 
-    bodyCoordinates = [[0,0],[0,1]] #starting body coordinates
+    bodyCoordinates = [[0,0],[0,1],[0,2]] #starting body coordinates
     white = (200,200,200)
     def __init__(self):
         self.length = 1
@@ -19,9 +19,6 @@ class Snake:
     
     def eat(self,animal):
         self.length += 1
-
-    def drawSnake():
-        pygame.draw.rect
 
     def turnSnakeHead(self,direction):
         keyPressed = pygame.key.get_pressed()
@@ -43,13 +40,13 @@ class Animal:
         
 
 class Grid:
-    
-
+    white = (200,200,200)
+    black = (0,0,0)
     def __init__(self):
         self.gridWidth = 500
         self.gridHeight = 500
         # self.grid = []
-        self.white = (200,200,200)
+        
         
 
     def createGrid(self,x,y):
@@ -61,15 +58,18 @@ class Grid:
 
     def drawRect(self, x = 40, y = 40, width = 20, height = 20, colour = (200,200,200)):#width and height set to default grid height
         rect = pygame.Rect(y, x, width, height)
-        pygame.draw.rect(window, (200,200,200), rect)
+        pygame.draw.rect(window, colour, rect)
     
-    def drawGrid(self, window):
+    def drawGrid(self, window, colour=(200,200,200)):
         # self.drawRect()
         for col in range(0,self.gridWidth,22):
             for row in range(0,self.gridHeight,22):
-                self.drawRect(col, row, 20, 20, (10,10,10))
+                self.drawRect(col, row, 20, 20, colour)
         # pygame.draw.rect(window, self.white, (10,10,10,10))
-
+    
+    def drawSnake(self, snake):
+        for x,y in snake:
+            self.drawRect(x*22, y*22 ,20,20, self.black)
 
 
 
@@ -77,8 +77,11 @@ class Grid:
 def draw_game():
     grid = Grid()
     grid.drawGrid(window)
+    snake = Snake()
+    coordinates = snake.bodyCoordinates
+    grid.drawSnake(coordinates)
     pygame.display.flip()
-
+    # pygame.display.update()
 run = True
 while run: 
     pygame.time.delay(50)
