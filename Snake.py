41f809bc7e379f09e839e85d0db91eec3654aadc
Z <zacharyk.ishmael@gmail.com>
2021-05-29 14:49:19 +0100

    Moved snake class to separate file

---
+++ Snake.py
@@ -0,0 +1,96 @@
+import pygame
+class Snake:
+    up = 1
+    down = 2
+    left = 3
+    right = 4
+    black=(0,0,0)
+    head = 0 #collumn for head of snake
+    x = 0 # key for x position of snake head
+    y = 1 # key for y position of snake head 
+    bodyCoordinates = [[0,0],[1,0],[2,0]] #starting body coordinates
+    white = (200,200,200)
+    def __init__(self):
+        self.length = 1
+        self.direction = 0
+
+    
+    def eat(self,animal):
+        print("Snake eats animal")
+        self.length += animal.points
+        self.bodyCoordinates.append([-1,-1])
+    
+    def drawSnake(self,grid,steps):
+        for x,y in self.bodyCoordinates:
+            grid.drawRect(x*steps, y*steps, 20, 20, self.black) #in steps of 22 pixels to allow the gaps
+    
+    #changes 'head' coordinate trajectory
+    def turnSnakeHead(self):
+        keyPressed = pygame.key.get_pressed()
+        if keyPressed[pygame.K_UP]: #TODO and not already moving upwards
+            self.bodyCoordinates[self.head][self.y] -= 1; # move the head of the snake up
+            self.direction = self.up #TODO uncomment to turn off free roam mode
+
+        if keyPressed[pygame.K_DOWN]: #TODO and not already heading downwards
+            self.bodyCoordinates[self.head][self.y] += 1; #turn snake head down
+            self.direction = self.down #TODO uncomment to turn off free roam mode
+
+        if keyPressed[pygame.K_LEFT]: #TODO and not already heading left
+            self.bodyCoordinates[self.head][self.x] -= 1; #turn snake head down
+            self.direction = self.left #TODO uncomment to turn off free roam mode
+        
+        if keyPressed[pygame.K_RIGHT]: #TODO and not already heading right
+            self.bodyCoordinates[self.head][self.x] += 1; #turn snake head down
+            self.direction = self.right #TODO uncomment to turn off free roam mode
+    
+    def getSnakeHead(self):
+        x = self.bodyCoordinates[self.head][self.x]
+        y = self.bodyCoordinates[self.head][self.y]
+        return x,y
+    
+    def getDirection(self):
+        return self.direction
+
+    def setSnakeHead(self, x, y):
+        self.bodyCoordinates[self.head][self.x] = x
+        self.bodyCoordinates[self.head][self.y] = y
+
+    #places new head block at the new coordinates
+    #assumes coordinates for head have been already changed
+    def placeHead(self):
+        #get head coordinates
+        x,y = self.getSnakeHead()
+
+        #get head direction
+        if self.direction == self.right:
+            x += 1
+
+        if self.direction == self.left:
+            x -= 1
+        
+        if self.direction == self.up:
+            y -= 1
+
+        if self.direction == self.down:
+            y += 1
+
+        #insert head
+        self.bodyCoordinates.insert(0,[x,y])
+    
+    #pop tail
+    def popTail(self,grid):
+        # x,y = self.bodyCoordinates[-1]
+        # grid.drawRect(x,y,20,20,self.white)
+        self.bodyCoordinates.pop()
+
+    #moves the snake a block forward in the current trajectory
+    def move(self,grid):
+        self.placeHead()
+        self.popTail(grid)
+        
+    def displayScore(self, window):
+        font = pygame.font.Font(None, 100)
+        text = font.render("Score:" + str(self.length), True, self.white)
+        textX = 800
+        textY = 50
+        window.blit(text,(textX,textY))
